4210900f588bcc8a810632a32f75f33bdd1147f2
ModerRAS <adqewsx@gmail.com>
2023-08-23 16:55:48 +0800

    添加OCR相关的测试

---
+++ SelectAndSearch.Test/OCRHookTest.cs
@@ -0,0 +1,25 @@
+﻿using SelectAndSearch.Hooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectAndSearch.Test {
+    [TestClass]
+    public class OCRHookTest {
+        [TestMethod]
+        public void TestOCR() {
+            var ocr = new OCRHook();
+            Task.Delay(1000).Wait();
+            ocr.Execute();
+        }
+        [TestMethod]
+        public void BenchmarkOCR() {
+            var ocr = new OCRHook();
+            for (int i = 0; i < 60; i++) {
+                ocr.Execute();
+            }
+        }
+    }
+}
