29c91fb14d82d581e016856ee599b67a0b1c69b4
ModerRAS <adqewsx@gmail.com>
2023-08-24 09:12:31 +0800

    添加性能测试

---
+++ SelectAndSearch.Benchmark/OCRHookBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SelectAndSearch.Common.Hooks;
+
+namespace SelectAndSearch.Benchmark {
+    public class OCRHookBenchmark {
+        [Benchmark]
+        public void BenchmarkGetScreenSize() {
+            var (x, y) = OCRHook.GetScreenSize();
+        }
+        [Benchmark]
+        public void BenchmarkGetScreenCapture() {
+            var bitmap = OCRHook.GetScreenCapture();
+        }
+        [Benchmark]
+        public void BenchmarkGetDPIScaling() {
+            var tmp = OCRHook.GetDPIScaling();
+        }
+    }
+}
---
+++ SelectAndSearch.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SelectAndSearch.Benchmark {
+    internal class Program {
+        public static void BenchmarkTest() {
+            BenchmarkRunner.Run<OCRHookBenchmark>();
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            BenchmarkTest();
+        }
+    }
+}
