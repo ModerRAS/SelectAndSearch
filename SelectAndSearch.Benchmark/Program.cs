366130a4012dcba8ef5f786a65d5a1cbd734e539
ModerRAS <adqewsx@gmail.com>
2023-08-27 10:15:55 +0800

    修复性能测试只用Release模式编译的问题

--- SelectAndSearch.Benchmark/Program.cs
+++ SelectAndSearch.Benchmark/Program.cs
@@ -1,14 +1,26 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using Sdcb.PaddleInference;
 
 namespace SelectAndSearch.Benchmark {
     internal class Program {
         public static void BenchmarkTest() {
-            BenchmarkRunner.Run<OCRHookBenchmark>();
+            BenchmarkRunner.Run<OCRHookBenchmark>(DefaultConfig.Instance.With(
+                    Job.Default.WithCustomBuildConfiguration(
+#if DEBUG || RELEASE_MKL || RELEASE
+                "Release_mkl"
+#elif RELEASE_OPENBLAS
+                "Release_openblas"
+#elif RELEASE_OPENBLAS_NOAVX
+                "Release_openblas_noavx"
+#endif
+                        )
+                    ));
         }
 
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
             BenchmarkTest();
         }
     }
