9e61cda80a1dad203a6c4fbc11aa0e94a94d25b7
ModerRAS <adqewsx@gmail.com>
2023-08-29 09:51:24 +0800

    添加一个新的测试

--- SelectAndSearch.Test/ExcelManagerTest.cs
+++ SelectAndSearch.Test/ExcelManagerTest.cs
@@ -7,7 +7,7 @@ namespace SelectAndSearch.Test {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
         }
         [TestMethod]
-        public void TestParseDocument() {
+        public void TestParseDocument_1() {
             var manager = new ExcelManager();
             var questions = manager.ParseDocument("TestData/ExcelManager/test1.xlsx");
             Assert.AreEqual(1, questions.Count);
@@ -17,6 +17,19 @@ namespace SelectAndSearch.Test {
             Assert.AreEqual("选项B", questions[0].Answers[1]);
             Assert.AreEqual("选项C", questions[0].Answers[2]);
             Assert.AreEqual(3, questions[0].Answers.Count);
+        }
+        [TestMethod]
+        public void TestParseDocument_2() {
+            var manager = new ExcelManager();
+            var questions = manager.ParseDocument("TestData/ExcelManager/test2.xlsx");
+            Assert.AreEqual(1, questions.Count);
+            Assert.AreEqual("麒麟操作系统中，运行级别（）是图形界面的多用户模式。", questions[0].Title);
+            Assert.AreEqual("C", questions[0].CorrectAnswer);
+            Assert.AreEqual("1", questions[0].Answers[0]);
+            Assert.AreEqual("3", questions[0].Answers[1]);
+            Assert.AreEqual("5", questions[0].Answers[2]);
+            Assert.AreEqual("7", questions[0].Answers[3]);
+            Assert.AreEqual(4, questions[0].Answers.Count);
         }
     }
 }
